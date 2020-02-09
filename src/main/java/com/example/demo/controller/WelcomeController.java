3b117fbac5d761d42b127eb26c8356207daaff3e
tatrunganh1996 <tatrunganha28@gmail.com>
2020-02-09 22:30:27 +0700

    add login controller

--- src/main/java/com/example/demo/controller/WelcomeController.java
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
@@ -34,5 +36,14 @@ public class WelcomeController {
         model.addAttribute("message", name);
 
         return "404";
+    }
+
+    @GetMapping("/login/{admin}")
+    public String login(
+            @PathVariable(name = "admin", required = false)
+                    String admin, Model model) {
+        model.addAttribute("admin", admin);
+
+        return "login";
     }
 }
